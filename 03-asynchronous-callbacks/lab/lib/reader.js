69806973e42c40b87be07b7dda72e4bb75e8babc
trevorstam <trevorstam@gmail.com>
2018-10-20 23:47:08 -0700

    something went wrong, it should be done now and pushed to github

--- 03-asynchronous-callbacks/lab/lib/reader.js
+++ 03-asynchronous-callbacks/lab/lib/reader.js
@@ -1,50 +1,35 @@
 'use strict';
 const fs = require('fs');
 
-const meenie = `${__dirname}/../data/meenie.txt`;
-const eenie = `${__dirname}/../data/eenie.txt`;
-const moe = `${__dirname}/../data/moe.txt`;
-
-let paths = [moe, meenie, eenie];
-
-// let data1 = fs.readFileSync(moe);
-
-// let data2 = fs.readFileSync(meenie);
-
-// let data3 = fs.readFileSync(eenie);
-
-const reader = ((paths, filesTotalRead)=>{
+const reader = module.exports = ((paths, readThemAll) => {
   const pushArray = [];
-  const moeRead = ((error, data1) =>{
-    if (error){
-      filesTotalRead(error);
-      return;
-    }
-    pushArray.push(null, data1.toString());
-    fs.readFile(paths[0], meenieRead);
-    
-  });
-  
-  const meenieRead =((error, data2) =>{
+  fs.readFile(paths[0], (error, data) => {
     if (error) {
-      filesTotalRead(error);
+      readThemAll(error);
       return;
+    } else {
+      pushArray.push(data.toString());
     }
-    pushArray.push(null, data2.toString());
-    fs.readFile(paths[1], eenieRead);
-  });
-  
-  const eenieRead = ((error, data3) => {
-    if (error){
-      filesTotalRead(error);
-      return;
-    }
-  });
 
-  fs.readFile(paths[2], eenieRead);
-  pushArray.push(null, data3.toString());
-  filesTotalRead(null, pushArray);
-  
+    fs.readFile(paths[1], (error, data) => {
+      if (error) {
+        readThemAll(error);
+        return;
+      } else {
+        pushArray.push(data.toString());
+      }
+
+      fs.readFile(paths[2], (error, data) => {
+        if (error) {
+          readThemAll(error);
+          return;
+        } else {
+          pushArray.push(data.toString());
+        }
+        readThemAll(null, pushArray);
+        return;
+      });
+    });
+  });
 });
 
-module.exports = reader;

e137722199f92ef628b868e0de0d9acaacab2e08
JB Tellez <jb.tellez@gmail.com>
2018-10-29 13:05:37 -0700

    removes errant comments

--- 11-express/lab/index.js
+++ 11-express/lab/index.js
@@ -2,11 +2,3 @@ require('dotenv').config();
 require('babel-register');
 
 require('./src/app.js').start(process.env.PORT);
-
-
-// import app from './src/app.js';
-// app.start(process.env.PORT);
-
-// import version from './src/app.js';
-
-// console.log('version', version)

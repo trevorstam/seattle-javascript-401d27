496283fa2bf9b59a3cf112f87332d45f29c3b432
trevorstam <trevorstam@gmail.com>
2018-10-29 18:16:08 -0700

    npm not installing packages, need reboot

--- 11-express/lab/index.js
+++ 11-express/lab/index.js
@@ -1,4 +1,11 @@
+'use strict';
+
 require('dotenv').config();
+
+
 require('babel-register');
 
-require('./src/app.js').start(process.env.PORT);
+import app from './src/app';
+app.start(process.env.PORT);
+
+import version from './src/app';
--- 11-express/lab/src/app.js
+++ 11-express/lab/src/app.js
@@ -4,13 +4,16 @@ import express from 'express';
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({
+  extended: true,
+}));
 
 // app.use(albumRouter);
+export const version = '1.0';
 
 module.exports = {
   app,
   start: (port) => {
     app.listen(port, () => console.log('Running on', port));
-  }
-}
+  },
+};

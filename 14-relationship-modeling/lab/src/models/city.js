c14d4bed5186d50c4437c3e0cd8ad567451e07f5
trevorstam <trevorstam@gmail.com>
2018-11-05 00:13:23 -0800

    routes updated, schema's updated with populate

--- 14-relationship-modeling/lab/src/models/city.js
+++ 14-relationship-modeling/lab/src/models/city.js
@@ -1,14 +1,17 @@
-import mongoose, {
-  Schema,
-} from 'mongoose';
+import mongoose from 'mongoose';
 
 const citySchema = mongoose.Schema({
   name: String,
   nickname: String,
   state: {
-    type: Schema.Types.ObjectID,
+    type: mongoose.Schema.Types.ObjectID,
     ref: 'state',
   },
+});
+
+citySchema.pre('findOne', (next) => {
+  this.populate('state');
+  next();
 });
 
 const city = mongoose.model('city', citySchema);

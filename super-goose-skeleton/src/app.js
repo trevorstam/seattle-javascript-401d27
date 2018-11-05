55ad8afd34af0b1421aba9b09b38470d04cdb79c
JB Tellez <jb.tellez@gmail.com>
2018-11-05 07:06:33 -0800

    adds app middleware

--- super-goose-skeleton/src/app.js
+++ super-goose-skeleton/src/app.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import notFound from './middleware/404.js';
+import error from './middleware/error.js';
 
 const app = express();
 
+// sign on life route, real routes should be in own router
 app.get('/ping', (request, response) => {
   response.send('pong');
 });
+
+app.use(notFound);
+app.use(error);
 
 let server;
 
